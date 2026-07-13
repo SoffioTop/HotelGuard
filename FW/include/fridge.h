425659d7065bb0941dbce03f272651c47a4f9167
SoffioTop <103445572+SoffioTop@users.noreply.github.com>
2026-07-13 18:42:37 +0200

    Update fridge.h

--- FW/include/fridge.h
+++ FW/include/fridge.h
@@ -14,6 +14,25 @@ class Fridge
 {
 public:
 
+    explicit Fridge(uint8_t pin);
+
+    void begin();
+    void update();
+
+    bool isOpen() const;
+
+    Defcon getDefcon() const;
+
+    unsigned long getOpenDuration() const;
+
 private:
 
+    uint8_t pin;
+
+    bool open;
+    bool previousOpen;
+
+    unsigned long openedAt;
+
+    Defcon defcon;
 };
