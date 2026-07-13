63545466cc8bc66ab4b32c3f509992912379e9cd
SoffioTop <103445572+SoffioTop@users.noreply.github.com>
2026-07-13 18:27:10 +0200

    Create fridge.h

---
+++ FW/include/fridge.h
@@ -0,0 +1,19 @@
+/*
+ * HotelGuard
+ * File: fridge.h
+ *
+ * Gestione di una singola cella frigorifera.
+ */
+
+#pragma once
+
+#include <Arduino.h>
+#include "defcon.h"
+
+class Fridge
+{
+public:
+
+private:
+
+};
