d93f6a6ffe7201d87747a77d3a5bdb4753063744
SoffioTop <103445572+SoffioTop@users.noreply.github.com>
2026-07-13 22:49:31 +0200

    Update main.cpp

--- FW/src/main.cpp
+++ FW/src/main.cpp
@@ -1,15 +1,34 @@
+#include <Arduino.h>
+
+#include "pins.h"
+#include "fridge.h"
+#include "alarm.h"
+
+Fridge fridge1(PIN_FRIDGE_1);
+Fridge fridge2(PIN_FRIDGE_2);
+
+Alarm alarm(PIN_BUZZER);
+
 void setup()
 {
-    Serial.begin(115200);
-
     fridge1.begin();
+    fridge2.begin();
+
+    alarm.begin();
 }
 
 void loop()
 {
     fridge1.update();
+    fridge2.update();
 
-    Serial.println(fridge1.getOpenDuration());
+    Defcon currentDefcon = Defcon::Normal;
 
-    delay(100);
+    if (fridge1.getDefcon() < currentDefcon)
+        currentDefcon = fridge1.getDefcon();
+
+    if (fridge2.getDefcon() < currentDefcon)
+        currentDefcon = fridge2.getDefcon();
+
+    alarm.update(currentDefcon);
 }
