1c2cc30dc25f3d92f7c791685d531ca93c66b4cd
SoffioTop <103445572+SoffioTop@users.noreply.github.com>
2026-07-13 18:22:58 +0200

    Create pins.h

---
+++ FW/include/pins.h
@@ -0,0 +1,27 @@
+/*
+ * HotelGuard
+ * File: pins.h
+ *
+ * Mappatura hardware dei pin ESP32.
+ */
+
+#pragma once
+
+// =========================
+// Alimentazione
+// =========================
+
+// Nessun pin dedicato
+
+// =========================
+// Celle frigorifere
+// =========================
+
+constexpr uint8_t PIN_FRIDGE_1 = 27;
+constexpr uint8_t PIN_FRIDGE_2 = 26;
+
+// =========================
+// Allarmi
+// =========================
+
+constexpr uint8_t PIN_BUZZER = 18;
